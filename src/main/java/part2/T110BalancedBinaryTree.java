dc33b707474a03d950639c44e6ab147eda52e36b
chaoyi66 <chaoyi66@live.com>
2016-05-24 14:03:08 +0800

    递归解决平衡二叉树判定问题

---
+++ src/main/java/part2/T110BalancedBinaryTree.java
@@ -0,0 +1,46 @@
+package part2;
+
+import shcell.commons.TreeNode;
+
+public class T110BalancedBinaryTree {
+
+	public static void main(String[] args) {
+		System.out.println();
+	}
+
+	public boolean isBalanced(TreeNode root) {
+		if (root == null)
+			return true;
+		if (Math.abs(height(root.left) - height(root.right)) <= 1)
+			return (isBalanced(root.left) && isBalanced(root.right));
+		return false;
+	}
+
+	public int height(TreeNode root) {
+		if (root == null)
+			return 0;
+		int left = height(root.left);
+		int right = height(root.right);
+		return (Math.max(left, right) + 1);
+
+	}
+
+	public boolean isBalanced1(TreeNode root) {
+		return getDepth(root) != -1;
+	}
+
+	public int getDepth(TreeNode root) {
+		if (root == null) {
+			return 0;
+		}
+		int left = getDepth(root.left);
+		if (left != -1) {
+			int right = getDepth(root.right);
+			if (right != -1) {
+				return Math.abs(left - right) <= 1 ? 1 + Math.max(left, right) : -1;
+			}
+		}
+		return -1;
+	}
+
+}
