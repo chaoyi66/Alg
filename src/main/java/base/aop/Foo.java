49060edb71b4865351b58e6922e06a000506beb3
chaoyi66 <chaoyi66@live.com>
2018-10-23 16:04:18 +0800

    时间测试

---
+++ src/main/java/base/aop/Foo.java
@@ -0,0 +1,37 @@
+package base.aop;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.Vector;
+
+// 测试类Foo
+public class Foo {
+
+    @Timer
+    public void algo1() {
+        ArrayList<Integer> l = new ArrayList<>();
+        for (int i = 0; i < 10000000; i++) {
+            l.add(1);
+        }
+    }
+
+    @Timer
+    public void algo2() {
+        LinkedList<Integer> l = new LinkedList<>();
+        for (int i = 0; i < 10000000; i++) {
+            l.add(1);
+        }
+    }
+
+    public void algo3() {
+        Vector<Integer> v = new Vector<>();
+        for (int i = 0; i < 10000000; i++) {
+            v.add(1);
+        }
+    }
+
+    public static void main(String[] foo){
+        TimerUtil tu = new TimerUtil();
+        tu.getTime();
+    }
+}
